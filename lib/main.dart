05d18521822557ddbdb10b0be3640afef064a6f5
Hemant Sharma <hemantbeast@gmail.com>
2022-08-26 16:38:50 +0530

    Initialize hydrated bloc
    Signed-off-by: Hemant Sharma <hemantbeast@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:news/utils/bloc_observer.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb ? HydratedStorage.webStorageDirectory : await getTemporaryDirectory(),
+  );
   Bloc.observer = NewsBlocObserver();
-  runApp(const MyApp());
+  HydratedBlocOverrides.runZoned(
+    () => runApp(const MyApp()),
+    storage: storage,
+  );
 }
 
 class MyApp extends StatelessWidget {
