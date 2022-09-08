4dfcbb7588d139bbced30931ea6e0105f6b9cfa0
Hemant Sharma <hemantbeast@gmail.com>
2022-09-08 12:50:30 +0530

    Implement deep link
     * use andro://andronerds.com/pageA for testing
     * change pageA with B,C,D to test different page
    
    Signed-off-by: Hemant Sharma <hemantbeast@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:app_links/app_links.dart';
 import 'package:deep_link/routes/app_router.dart';
+import 'package:deep_link/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:navigation_history_observer/navigation_history_observer.dart';
 
@@ -6,10 +10,31 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Uri? _initialUrl;
+  Uri? _currentUrl;
+  StreamSubscription? _streamSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _initURIHandler();
+    _incomingLinkHandler();
+  }
+
+  @override
+  void dispose() {
+    _streamSubscription?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,5 +47,112 @@ class MyApp extends StatelessWidget {
       onGenerateRoute: AppRouter.generateRoute,
       navigatorObservers: [NavigationHistoryObserver()],
     );
+  }
+
+  void _initURIHandler() async {
+    try {
+      final appLinks = AppLinks();
+      final initialUrl = await appLinks.getInitialAppLink();
+      // Use the initialUrl and warn the user if it is not correct,
+      // but keep in mind it could be `null`.
+      if (initialUrl != null) {
+        debugPrint("Initial URI received $initialUrl");
+        if (!mounted) {
+          return;
+        }
+        setState(() {
+          _initialUrl = initialUrl;
+
+          if (_initialUrl != null) {
+            switch (_initialUrl!.path) {
+              case Routes.pageB:
+                AppRouter.pushNamed(Routes.pageB);
+                break;
+              case Routes.pageC:
+                AppRouter.pushNamed(Routes.pageB);
+                AppRouter.pushNamed(Routes.pageC);
+                break;
+              case Routes.pageD:
+                AppRouter.pushNamed(Routes.pageB);
+                AppRouter.pushNamed(Routes.pageC);
+                AppRouter.pushNamed(Routes.pageD);
+                break;
+            }
+          }
+        });
+      } else {
+        debugPrint("Null Initial URI received");
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  void _incomingLinkHandler() {
+    final appLinks = AppLinks();
+    _streamSubscription = appLinks.uriLinkStream.listen((Uri? uri) {
+      if (!mounted) {
+        return;
+      }
+      debugPrint('Received URI: $uri');
+      setState(() {
+        _currentUrl = uri;
+
+        if (_currentUrl != null) {
+          var history = NavigationHistoryObserver().history;
+          var lastRouteName = NavigationHistoryObserver().history.last.settings.name;
+
+          switch (_currentUrl!.path) {
+            case Routes.pageA:
+              if (lastRouteName != Routes.pageA) {
+                AppRouter.popUntilRoot();
+              }
+              break;
+            case Routes.pageB:
+              if (lastRouteName != Routes.pageB) {
+                if (history.any((page) => page.settings.name == Routes.pageB)) {
+                  AppRouter.popUntil(Routes.pageB);
+                } else {
+                  AppRouter.pushNamed(Routes.pageB);
+                }
+              }
+              break;
+            case Routes.pageC:
+              if (lastRouteName != Routes.pageC) {
+                if (history.any((page) => page.settings.name == Routes.pageC)) {
+                  AppRouter.popUntil(Routes.pageC);
+                } else {
+                  if (lastRouteName == Routes.pageA) {
+                    AppRouter.pushNamed(Routes.pageB);
+                  }
+                  AppRouter.pushNamed(Routes.pageC);
+                }
+              }
+              break;
+            case Routes.pageD:
+              if (lastRouteName == Routes.pageD) {
+                return;
+              }
+
+              if (lastRouteName == Routes.pageA) {
+                AppRouter.pushNamed(Routes.pageB);
+                AppRouter.pushNamed(Routes.pageC);
+              } else if (lastRouteName == Routes.pageB) {
+                AppRouter.pushNamed(Routes.pageC);
+              }
+              AppRouter.pushNamed(Routes.pageD);
+              break;
+          }
+        }
+      });
+    }, onError: (Object err) {
+      if (!mounted) {
+        return;
+      }
+      debugPrint('Error occurred: $err');
+      setState(() {
+        _currentUrl = null;
+      });
+    });
   }
 }
